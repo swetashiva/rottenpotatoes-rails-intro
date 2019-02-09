73b431a75a53b14bb1925409d427b90d2a2aaa4e
EC2 Default User <ec2-user@ip-172-31-30-60.us-east-2.compute.internal>
2019-02-09 19:01:28 +0000

    trying to sort by title

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,14 @@
 class Movie < ActiveRecord::Base
+   def sort_by_title(movies)
+    for i in 0 .. movies.length
+    for j in i .. movies.length
+        if(movies[i].title < movies[j].title)
+            string temp;
+            temp = movies[i].title;
+            movies[i].title=movies[j].title;
+            movies[j].title=temp;
+        end
+    end
+    end
+   end
 end
