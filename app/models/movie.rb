d3ce8333353d94a5875a8e6d57cedc5659fd0f19
EC2 Default User <ec2-user@ip-172-31-30-60.us-east-2.compute.internal>
2019-02-10 02:01:29 +0000

    testing part 2 v1

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,10 +1,4 @@
 class Movie < ActiveRecord::Base
-  attr_accessible :title, :rating, :description, :release_date, :director
-  def self.all_ratings
-  	result = {}
-  	self.select(:rating).uniq.each do |movie|
-  		result[movie.rating] = 1
-  	end
-  	return result
-  end
+
+
 end
