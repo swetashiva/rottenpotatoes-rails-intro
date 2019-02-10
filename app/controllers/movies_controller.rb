839a522414b1db3f1b88bbb1a3eb64c59670df7e
EC2 Default User <ec2-user@ip-172-31-30-60.us-east-2.compute.internal>
2019-02-10 00:28:53 +0000

    testing part 2 v1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,7 +13,7 @@ class MoviesController < ApplicationController
     def index
    
     @all_ratings = ['G','PG','PG-13','R']
-   
+    
     @movies = Movie.all
     
     #sorting the rating parameters
@@ -31,8 +31,7 @@ class MoviesController < ApplicationController
       session[:ratings] = @ratings_params
     end
     
-    @movies = Movie.find(params[:ratings])
-   
+    @movies = @movies.where('rating in (?)', @ratings_params)
     
     #storing the sorting parameters
     if params[:sort]
