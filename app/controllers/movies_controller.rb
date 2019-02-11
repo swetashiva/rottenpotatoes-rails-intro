bdf4cd8b85f13c0bce9be7aa8e49002057a78131
EC2 Default User <ec2-user@ip-172-31-30-60.us-east-2.compute.internal>
2019-02-11 14:22:10 +0000

    testing part 2 v1

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,47 +11,49 @@ class MoviesController < ApplicationController
   end
   
   def index
+   
     @all_ratings = ['G','PG','PG-13','R']
+    
     @movies = Movie.all
-    @ratings_params=all_ratings
     
+    if (params[:ratings] || params[:sort])
     #sorting the rating parameters
-    if params[:ratings]
-      @ratings_params = params[:ratings].keys
-    else
-      if session[:ratings]
-        @ratings_params = session[:ratings]
+      if params[:ratings]
+          @ratings_params = params[:ratings].keys
       else
-        @ratings_params = @all_ratings
+        if session[:ratings]
+          @ratings_params = session[:ratings]
+        else
+          @ratings_params = @all_ratings
+        end
       end
-    end
-    
-    if @ratings_params!=session[:ratings]
-      session[:ratings] = @ratings_params
-    end
-    
-    @movies = @movies.where('rating in (?)', @ratings_params)
+        
+      if @ratings_params!=session[:ratings]
+        session[:ratings] = @ratings_params
+      end
+        
+      @movies = @movies.where('rating in (?)', @ratings_params)
     
     #storing the sorting parameters
-    if params[:sort]
-      @sorting_params = params[:sort]
-    else
-      @sorting_params = session[:sort]
+    elsif params[:sort]
+        @sorting_params = params[:sort]
+
+      if @sorting_params!=session[:sort]
+        session[:sort] = @sorting_params
+      end
+        
+      if @sorting_params == 'title'
+            @movies = @movies.order(@sorting_params)
+            @sort_by_title = 'hilite'
+      elsif @sorting_params == 'release_date'
+            @movies = @movies.order(@sorting_params)
+            @sort_by_release_date = 'hilite'
+      else  @movies= Movie.all
+      end
+    else  
+      @movies= Movie.all
+      
     end
-    
-    if @sorting_params!=session[:sort]
-      session[:sort] = @sorting_params
-    end
-    
-    if @sorting_params == 'title'
-          @movies = @movies.order(@sorting_params)
-          @sort_by_title = 'hilite'
-    elsif @sorting_params == 'release_date'
-          @movies = @movies.order(@sorting_params)
-          @sort_by_release_date = 'hilite'
-    else  @movies= Movie.all
-   end
-    
   end
 
   def new
